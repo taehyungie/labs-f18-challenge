3fc0814dd0b8df034d6bb10e6cbc066b33f57e9e
Ana Leon <al3607@barnard.edu>
2018-09-02 11:39:39 -0400

    So I changed the get request to include '+ query' at the end of the URL so the user can put whatever they'd like. It fetches the correct information from the API. Now I need to format it so the user sees in the browser.

--- app.py
+++ app.py
@@ -9,23 +9,23 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/pokemon/<int:query>', methods=['GET'])
+@app.route('/pokemon/<string:query>', methods=['GET'])
 def show_content(query):
-	r = requests.get('http://pokeapi.co/api/v2/pokemon/')
+	r = requests.get('http://pokeapi.co/api/v2/pokemon-form/'+ query)
 	print(r.json())
 	return render_template('test.html')
 
-@app.route('/pokemon/<string:query>', methods=['GET'])
-def show_content2(query):
-	s = requests.get('http://pokeapi.co/api/v2/pokemon-form/pikachu')
-	print(s.json())
-	return render_template('test.html')
+#@app.route('/pokemon/<string:query>', methods=['GET'])
+#def show_content2(query):
+#	s = requests.get('http://pokeapi.co/api/v2/pokemon-form/ + query')
+#	print(s.json())
+#	return render_template('test.html')
 
 if __name__ == '__show_content__':
     app.run() 
 
-if __name__ == '__show_content2__':
-    app.run()
+#if __name__ == '__show_content2__':
+#    app.run()
 
 if __name__ == '__main__':
     app.run()
