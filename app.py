eee08b4c8db9676a7598f782f32025a4e5ff476a
Ana Leon <al3607@barnard.edu>
2018-09-01 22:19:06 -0400

    trying to see if I can model the request to look like the request to get the index...May try something else

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+# @app.route('/pokemon/<query>', methods=['GET'])
+# def show_content(query):
+# 	return 
 
 if __name__ == '__main__':
     app.run()
