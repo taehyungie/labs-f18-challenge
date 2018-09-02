5885658d08649cece06f9158ec10f7518cc68128
Ana Leon <al3607@barnard.edu>
2018-09-02 01:14:34 -0400

    I am printing out the json contents when I sent the request... whenever I specify within the URL, such as /pikachu, it gives me the json file for pikachu vise versa with the integers...

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -7,12 +9,23 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/pokemon/<query>', methods=['GET'])
+@app.route('/pokemon/<int:query>', methods=['GET'])
 def show_content(query):
-	return render_template('test.html',)
+	r = requests.get('http://pokeapi.co/api/v2/pokemon/')
+	print(r.json())
+	return render_template('test.html')
+
+@app.route('/pokemon/<string:query>', methods=['GET'])
+def show_content2(query):
+	s = requests.get('http://pokeapi.co/api/v2/pokemon-form/pikachu')
+	print(s.json())
+	return render_template('test.html')
 
 if __name__ == '__show_content__':
     app.run() 
+
+if __name__ == '__show_content2__':
+    app.run()
 
 if __name__ == '__main__':
     app.run()
